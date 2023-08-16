411081f12fd7df20962d5aa3827fc585c1843a11
Sarah-faisal <sarahpervaizpk@gmail.com>
2023-08-16 16:09:02 +0500

    git fetch for a single object completed

--- src/component/flightsCards.js
+++ src/component/flightsCards.js
@@ -2,17 +2,16 @@ import React from 'react'
 import DefaultImg from './plane.png'
 
 export default function flightsCards(props) {
-    console.log(props.data.cardTitle)
 
   return (
     <div style={{width:`${props.width}` , height:`${props.height}`}} className={`my2 cardRow ${props.displayStyle}`}>
-      <img src={DefaultImg} alt="img" className='cardTitleImg' />
+      <img src={props.data.cardTitleImg?props.data.cardTitleImg:DefaultImg} alt="img" className='cardTitleImg' />
       <div className='cardDetails column'>
             <small className='cardSubTitle2'>{props.data.cardSubTitle2}</small>
             <p className='cardTitle'>{props.data.cardTitle}</p>
             <p className='cardSubTitle1'>{props.data.cardSubTitle1}</p>
             <p className='cardDescription'>{props.data.cardDescription}</p>
-            <a className='cardDesLink' href="sarahweb.info">{props.data.cardDesLink}</a>             
+            <a className='cardDesLink' href={props.data.cardDesLink}>More..</a>             
       </div>
     </div>
   );
@@ -28,6 +27,6 @@ flightsCards.defaultProps = {
         cardSubTitle1 : "",
         cardSubTitle2 : "",
         cardDescription : "",
-        cardDesLink : ""
+        cardDesLink : "https://www.tripadvisor.com/"
         }
   };
--- src/component/grid.js
+++ src/component/grid.js
@@ -1,30 +1,52 @@
-import React from 'react'
-import './dashboard.css'
+import React, { useEffect, useState } from 'react';
 import FlightCard from './flightsCards'
 
-export default function grid() {
-    let data = {
+export default function Grid() {
+  const [data, setData] = useState(
+    {
         cardTitleImg :"",
-        cardTitle : "AnB Flights To Dubai",
-        cardSubTitle1 : "Economoy",
-        cardSubTitle2 : "Specail Offer",
-        cardDescription : "This is a limited time deal for valued Customers of AnB",
-        cardDesLink : "See here"
+        cardTitle : "",
+        cardSubTitle1 : "",
+        cardSubTitle2 : "",
+        cardDescription : "",
+        cardDesLink : "https://www.tripadvisor.com/"
+    }
+  );
+  
+const getData =()=>{
+    let place = "Egypt";
+    const url = "https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query="+place+"%20tower&lang=en_US&units=km";
+  const options = {
+      method: 'GET',
+      headers: {
+          'X-RapidAPI-Key': '10a4ba1cd4msh1ea4162312a54efp1c67bdjsnec864ab7b06e',
+          'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
       }
-    
+  };	
+
+  fetch(url, options)
+  .then(response => response.json())
+  .then(nData => {
+     let newData = nData.data.Typeahead_autocomplete.results;
+      let newObj = {
+        cardTitleImg :newData[0].image.photo.photoSizes[1].url?newData[0].image.photo.photoSizes[1].url:"newData[0].route."+"www.tripadvisor.com"+".url",
+        cardTitle : newData[0].detailsV2.names.name?newData[0].detailsV2.names.name:newData[0].text,
+        cardSubTitle1 : newData[0].detailsV2.names.longOnlyHierarchyTypeaheadV2?newData[0].detailsV2.names.longOnlyHierarchyTypeaheadV2:newData[0].parentGeoDetails.names.longOnlyHierarchyTypeaheadV2,
+        cardSubTitle2 : newData[0].detailsV2.placeType?newData[0].detailsV2.placeType:newData[0].buCategory,
+        cardDescription : "",
+        cardDesLink : "https://www.tripadvisor.com/"
+    }
+    setData(newObj);  
+    console.log(newData.length)
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+}
   return (
     <div className='grid'>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
-        <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
+        <button onClick={getData}>get Data</button>
+      <FlightCard displayStyle='portrait' width='20vw' height="25vw" data={data}/>
     </div>
-  )
+  );
 }
